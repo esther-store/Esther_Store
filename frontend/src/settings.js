54a8e986ae5c84a701121a50f90bb1f8934de314
Lazaro Altedill <lazaroah02@gmail.com>
2025-01-12 15:45:59 -0500

    reading .evn file with astro

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:8000';
+export const BASE_URL = import.meta.env.PUBLIC_API_URL;
 
 //store urls
 export const URL_STORE_GET_CATEGORIES= `${BASE_URL}/store/categories/`
