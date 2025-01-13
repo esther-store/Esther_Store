891d2f3953bd6643f6e313eb70b976eb5990d223
Lazaro Altedill <lazaroah02@gmail.com>
2025-01-13 08:59:35 -0500

    adding react-query to useGetPromotions hook

--- frontend/src/hooks/useGetPromotions.js
+++ frontend/src/hooks/useGetPromotions.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { getPromotions } from "../services/ManagePromotions/getPromotions";
-import AuthenticationContext from '../context/authenticationContext.jsx'
+import AuthenticationContext from "../context/authenticationContext.jsx";
+import { useQuery } from "@tanstack/react-query";
 
-export function useGetPromotions({searchParams, setNumOfPromotions,setPromotions}) {
-    const {auth} = useContext(AuthenticationContext)
-    const [loading, setLoading] = useState(false);
-    const [promotions, changePromotions] = useState([])
-    //get promotions of store
-    useEffect(() => {
-        setLoading(true);
-        getPromotions(searchParams,auth.token)
-        .then((data) => {
-            setPromotions(data);
-            changePromotions(data);
-            setNumOfPromotions(data.count);
-            setLoading(false);
-        })
-        .catch(() => {
-            setLoading(false);
-            setNumOfPromotions(0)   
-        });
-    }, [searchParams]);
+export function useGetPromotions({
+  searchParams,
+  setNumOfPromotions,
+  setPromotions,
+}) {
+  const { auth } = useContext(AuthenticationContext);
+  const {
+    data,
+    isLoading: loading,
+    isError,
+  } = useQuery({
+    queryKey: ["promotions", searchParams.toString(), auth.token],
+    queryFn: () => getPromotions(searchParams.toString(), auth.token),
+  });
+  const promotions = data || [];
+  //get promotions of store
+  useEffect(() => {
+    if (data) {
+      setPromotions(data);
+      setNumOfPromotions(data.count);
+    }
+  }, [data]);
 
-    return({promotions,loading,setLoading})
-
+  return { promotions, loading, setLoading: () => {} };
 }
-
-
