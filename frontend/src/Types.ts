08bc5696544a9da4e2c7361ab8d9ac4896c0437f
Lazaro Altedill <lazaroah02@gmail.com>
2025-01-18 19:48:57 -0500

    creating types for product, category and promotion

--- frontend/src/Types.ts
+++ frontend/src/Types.ts
@@ -1,15 +1,40 @@
-interface ProductType{
-    id:null,
-    product_name: "",
-    product_description: "",
-    precio: 0,
-    categoria: null,
-    promotion:null,
-    is_active: true,
-    recommended:false,
-    in_stock: 0,
-    descuento: 0,
-    product_img1: null,
-    product_img2: null,
-    product_img3: null,
+type CategoryType = {
+    id: number,
+    nombre: string,
+    img: string,
+    created_at: string,
+    updated_at: string
+}
+type PromotionType = {
+    id: number,
+    cantidad_products: number,
+    created_at: string,
+    name: string,
+    description?: string,
+    discount_in_percent: number,
+    img: string,
+    active: boolean,
+    is_special: boolean,
+    updated_at: string
+}
+type ProductType = {
+    id: number,
+    product_name: string,
+    categoria?: number,
+    promotion?: number,
+    categoria_full_info: CategoryType,
+    promotion_full_info: ProductType,
+    puntuacion: number,
+    cantidad_puntuaciones: number,
+    product_description?: string,
+    precio: number,
+    is_active: boolean,
+    recommended: boolean,
+    in_stock: number,
+    descuento: number,
+    product_img1: string,
+    product_img2?: string,
+    product_img3?: string,
+    updated_at: string,
+    created_at: string
 }
