313d804f5912f06eb0ad295d53ef0dcd2003c909
Lazaro Altedill <lazaroah02@gmail.com>
2025-01-30 08:26:47 -0500

    moving Footer to index.astro to evoid initial blank page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import NavBar from "./components/NavBar/index.jsx";
-import Footer from "./components/Footer/index.jsx";
 import "primeicons/primeicons.css";
 import "./index.css";
 import AppRoutes from "./AppRoutes.jsx";
@@ -17,9 +16,6 @@ function App() {
         <section className="main-section-route">
           <AppRoutes/>
         </section>
-        <footer>
-          <Footer />
-        </footer>
       </section>
     </Providers>
   );
