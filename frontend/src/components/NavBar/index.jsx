18d8d2b1e98e782847e9ce6cf469d8c89c4fb3eb
Lazaro Altedill <lazaroah02@gmail.com>
2025-02-03 16:04:24 -0500

    showing links to navigate through home page in navbar

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -13,27 +13,31 @@ function NavBar() {
   return (
     <nav className="navbar">
       <ul>
-        <li className = "navbar-logo-container">
-          <Link to = "/">
+        <li className="navbar-logo-container">
+          <Link to="/">
             <CompanyLogo />
           </Link>
         </li>
-        <li className = "navbar-promotions-container">
-          
+        {pathname === "/" ? (
+          <>
+            <li className="navbar-promotions-container">
+              <a className = "navbar-homepage-link">Promociones</a>
+            </li>
+            <li className="navbar-recommended-products-container">
+              <a className = "navbar-homepage-link">Productos Recomendados</a>
+            </li>
+            <li className="navbar-categories-container ">
+              <a className = "navbar-homepage-link">Categorías</a>
+            </li>
+          </>
+        ) : null}
+        <li className="navbar-search-container">
+          <Search redirectToStoreOnSearch={true} />
         </li>
-        <li className = "navbar-recommended-products-container">
-          
+        <li className="navbar-cart-container">
+          <Cart />
         </li>
-        <li className = "navbar-categories-container">
-          
-        </li>
-        <li className = "navbar-search-container">
-          <Search redirectToStoreOnSearch = {true}/>
-        </li>
-        <li className = "navbar-cart-container">
-          <Cart/>
-        </li>
-        <li className = "navbar-dropdown-container">
+        <li className="navbar-dropdown-container">
           <NavbarDropdown />
         </li>
       </ul>
