1063b627a999c3151bc410d94e6b1a3796d2ae3a
Lazaro Altedill <lazaroah02@gmail.com>
2025-02-05 15:06:55 -0500

    finishing responsive desing of promotins and product card on homepage

--- frontend/src/components/HomePageComponents/Promotions/index.jsx
+++ frontend/src/components/HomePageComponents/Promotions/index.jsx
@@ -1,21 +1,31 @@
-import './index.css'
-import { useGetPromotions } from '@/hooks/useGetPromotionsFromProducts'
-import { useGetProducts } from '@/hooks/useGetProducts'
-import ProductCard from '@/components/StorePageComponents/ProductsGrid/ProductCard'
-import { useRef } from 'react'
-import { Toast } from 'primereact/toast'
+import "./index.css";
+import { useGetPromotions } from "@/hooks/useGetPromotionsFromProducts";
+import { useGetProducts } from "@/hooks/useGetProducts";
+import ProductCard from "@/components/StorePageComponents/ProductsGrid/ProductCard";
+import { useRef } from "react";
+import { Toast } from "primereact/toast";
 
-export function HomePagePromotions(){
-    const toastRef = useRef()
-    const {promotions, loadingPromotions} = useGetPromotions()
-    const {products} = useGetProducts({searchParams:`promotion=${promotions[0].id}&page_size=3`})
-    return(
-        <article className = "homepage-promotions-section">
-            <Toast ref = {toastRef} position="bottom-center"/>
-            <h1>Promociones</h1>
-            <section className = "cards-container">
-                {products.map(product => <ProductCard key = {product.id} product = {product} toastRef = {toastRef} onClick = {() => {}}/>)}
-            </section>
-        </article>
-    )
+export function HomePagePromotions() {
+  const toastRef = useRef();
+  const { promotions, loadingPromotions } = useGetPromotions();
+  const { products } = useGetProducts({
+    searchParams: `promotion=${promotions[0].id}&page_size=3`,
+  });
+  return (
+    <article className="homepage-promotions-section">
+      <Toast ref={toastRef} position="bottom-center" />
+      <h1>Promociones</h1>
+      <section className="cards-container">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            toastRef={toastRef}
+            onClick={() => {}}
+            showAddToCartButton = {false}
+          />
+        ))}
+      </section>
+    </article>
+  );
 }
--- frontend/src/components/StorePageComponents/ProductsGrid/ProductCard/index.jsx
+++ frontend/src/components/StorePageComponents/ProductsGrid/ProductCard/index.jsx
@@ -3,24 +3,29 @@ import React from "react";
 import AddToCartButton from "@/components/Cart/AddToCartButton";
 import ProductPrice from "./ProductPrice";
 
-const ProductCard = React.memo(function ProductCard({toastRef, product, onClick }) {
+const ProductCard = React.memo(function ProductCard({
+  toastRef,
+  product,
+  onClick,
+  showAddToCartButton = true,
+}) {
   return (
     <article className="product-card">
       <header className="img-container" onClick={onClick}>
         <img src={product.product_img1} alt={product.product_name} />
       </header>
-      <section
-        className="name-and-price-container"
-        onClick={onClick}
-      >
+      <section className="name-and-price-container" onClick={onClick}>
         <h1 className="product-card-name"> {product.product_name} </h1>
+        <div className="product-card-punctuation"></div>
         <ProductPrice
           precio={product.precio}
           price_with_discounts={product.price_with_discounts}
         />
       </section>
       <div className="add-to-cart-button-container">
-        <AddToCartButton product={product} toastRef = {toastRef}/>
+        {showAddToCartButton === true ? (
+          <AddToCartButton product={product} toastRef={toastRef} />
+        ) : null}
       </div>
     </article>
   );
