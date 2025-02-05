a613a417beb5c118f585294dc3396901893fdd47
Lazaro Altedill <lazaroah02@gmail.com>
2025-02-04 21:35:04 -0500

    homepage categories grid finished

--- frontend/src/components/HomePageComponents/CategoriesGrid/index.tsx
+++ frontend/src/components/HomePageComponents/CategoriesGrid/index.tsx
@@ -7,10 +7,15 @@ export function CategoriesGrid(){
     const {categories, loading, isError, refetch} = useGetCategories()
     return(
         <article className = "homepage-categories-grid-container">
-            <main className = "homepage-categories-grid">
+            <header>
+                <h1>Categorias</h1>
+                <h3>Encuentra facilmente lo que buscas</h3>
+            </header>
+            <main className = "homepage-categories-grid"> 
                 {categories.map(category => 
                 <Link to = {`/store?categoria=${category.id}`}>
                     <img src = {category.img}/>
+                    <div>{category.nombre}</div>
                 </Link>
                 )}
             </main>
