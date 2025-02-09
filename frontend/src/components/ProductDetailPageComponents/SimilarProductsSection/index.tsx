e2f67920ba11a169963bbdf62d061848e11432fe
Lazaro Altedill <lazaroah02@gmail.com>
2025-02-09 14:03:20 -0500

    showing error message on similar products

--- frontend/src/components/ProductDetailPageComponents/SimilarProductsSection/index.tsx
+++ frontend/src/components/ProductDetailPageComponents/SimilarProductsSection/index.tsx
@@ -4,41 +4,80 @@ import { useGetProducts } from "@/hooks/useGetProducts";
 import ProductCard from "@/components/StorePageComponents/ProductsGrid/ProductCard";
 import { Skeleton } from "primereact/skeleton";
 import { useNavigate } from "react-router-dom";
+import React, { Suspense } from "react";
+const RetryQueryComponent = React.lazy(
+  () => import("@/components/RetryQueryComponent")
+);
 
 export function SimilarProductsSection({
   currentProductId,
   filter,
   toastRef,
 }: {
-  currentProductId: ProductIdType
+  currentProductId: ProductIdType;
   filter: string;
   toastRef: any;
 }) {
-  const { products, loading, isError } = useGetProducts({
+  const { products, loading, isError, refetch } = useGetProducts({
     searchParams: filter,
   });
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <article className = "similar-products-section">
+    <article className="similar-products-section">
       <header>
         <h1>Productos Similares</h1>
       </header>
       <section>
-        {loading?
-        <>
-            <Skeleton style = {{maxWidth: '280px', maxHeight: '420px'}}/>
-            <Skeleton style = {{maxWidth: '280px', maxHeight: '420px'}}/>
-            <Skeleton style = {{maxWidth: '280px', maxHeight: '420px'}}/>
-            <Skeleton style = {{maxWidth: '280px', maxHeight: '420px'}}/>
-        </>
-        :
-        products.map((product: ProductType) => {
-          if(product.id != currentProductId){
-            return (<ProductCard key={product?.id} product = {product} toastRef={toastRef} onClick = {() => navigate(`/store/product/${product?.id}`)}/>)
-          }
-      })
-        }
+        {loading ? (
+          <CardsSkeleton />
+        ) : isError ? (
+          <Suspense fallback={<CardsSkeleton />}>
+            <div style = {styles.container}>
+              <RetryQueryComponent
+                message={
+                  "Error obteniendo los productos. Revisa tu conexión a internet"
+                }
+                refetch={refetch}
+              />
+            </div>
+          </Suspense>
+        ) : (
+          products.map((product: ProductType) => {
+            if (product.id != currentProductId) {
+              return (
+                <ProductCard
+                  key={product?.id}
+                  product={product}
+                  toastRef={toastRef}
+                  onClick={() => navigate(`/store/product/${product?.id}`)}
+                />
+              );
+            }
+          })
+        )}
       </section>
     </article>
   );
 }
+
+const CardsSkeleton = () => (
+  <>
+    <Skeleton height="280px" />
+    <Skeleton height="280px" />
+    <Skeleton height="280px" />
+    <Skeleton height="280px" />
+  </>
+);
+
+const styles = {
+  container: {
+    width: "95%",
+    minHeight: "200px",
+    position: "absolute",
+    top: "50%",
+    left:"50%",
+    transform: "translate(-50%, -50%)",
+    display: "flex",
+    alignItems: "center",
+  },
+};
