9f7d7d03f8c6fddb20286115d14843a428734492
Lazaro Altedill <lazaroah02@gmail.com>
2025-02-16 13:58:39 -0500

    stylizing bye page

--- frontend/src/store_pages/Bye.jsx
+++ frontend/src/store_pages/Bye.jsx
@@ -1,18 +1,44 @@
-import { RemovePageLoader } from '@/components/RemovePageLoader';
+import { RemovePageLoader } from '@/components/RemovePageLoader'
 import {Link} from 'react-router-dom'
 
 function Bye() {
     return ( 
-        <section className="protected-route-page">
+        <>
             <RemovePageLoader/>
-            <article>
+            <article style = {styles.article}>
                 <header>Vuelve Pronto 👋</header>
-                    <div>
-                        <Link to="/">Inicio</Link>
+                    <div style = {styles.div}>
+                        <Link style = {styles.a} to="/">Inicio</Link>
                     </div>
             </article>
-        </section>
-     );
+        </>
+     )
 }
 
-export default Bye;
+export default Bye
+
+const styles = {
+    article:{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        fontFamily: 'Poppins-Regular',
+        gap:'10px',
+        color: 'rgba(0, 0, 0, 0.8)',
+        position:"absolute",
+        left:"50%",
+        top:"50%",
+        transform:"translate(-50%, -50%)"
+    },
+    
+    div:{
+        backgroundColor: '#D9658F',
+        padding: '10px',
+        borderRadius: '5px',
+        textAlign:"center"
+    },
+    
+    a:{
+        color: 'white',
+    }
+}
