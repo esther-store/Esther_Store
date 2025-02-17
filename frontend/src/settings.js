07da292ae49051150f80ae104f2ec1b44ec2fe77
Lazaro Altedill <lazaroah02@gmail.com>
2025-02-17 16:52:26 -0500

    adjusting product detail pages styles

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -9,9 +9,7 @@ export const URL_GET_PROMOTIONS = `${BASE_URL}/store/promotions/`
 export const URL_MANAGE_PRODUCTS = `${BASE_URL}/store/manage-products/`
 export const URL_MANAGE_CATEGORIES = `${BASE_URL}/store/manage-categories/`
 export const URL_MANAGE_PROMOTIONS = `${BASE_URL}/store/manage-promotions/`
-
 export const URL_MANAGEMENT_USERS = `${BASE_URL}/user/managment/`
-
 
 //authentication
 export const LOGIN_URL = `${BASE_URL}/authentication/login/`
@@ -19,6 +17,7 @@ export const CHANGE_PASSWORD_URL = `${BASE_URL}/authentication/password/change/`
 
 //contact info
 export const CONTACT_INFO_URL = `${BASE_URL}/contact-info/`
+export const MANAGE_CONTACT_INFO_URL = `${BASE_URL}/contact-info/manage/`
 
 //user profile
 export const USER_PROFILE_URL = `${BASE_URL}/authentication/user/`
