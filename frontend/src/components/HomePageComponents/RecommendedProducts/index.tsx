9aabe0950d716c7734b4cf1fa46c1e12084588ad
Lazaro Altedill <lazaroah02@gmail.com>
2025-02-21 19:06:11 -0500

    replacing products recommended homepage placeholder text with real one

--- frontend/src/components/HomePageComponents/RecommendedProducts/index.tsx
+++ frontend/src/components/HomePageComponents/RecommendedProducts/index.tsx
@@ -2,9 +2,11 @@ import "./index.css";
 import { useGetProducts } from "@/hooks/useGetProducts";
 import ImageSlider from "@/components/ImageSlider";
 import { useNavigate } from "react-router-dom";
-import { Skeleton } from 'primereact/skeleton';
+import { Skeleton } from "primereact/skeleton";
 import { lazy, Suspense } from "react";
-const RetryQueryComponent = lazy(() => import("@/components/RetryQueryComponent"));
+const RetryQueryComponent = lazy(
+  () => import("@/components/RetryQueryComponent")
+);
 
 export function RecommendedProducts() {
   const { products, loading, isError, refetch } = useGetProducts({
@@ -15,17 +17,27 @@ export function RecommendedProducts() {
     <article className="homepage-recommended-products">
       <aside>
         {loading ? (
-          <Skeleton width="100%" height="400px" className="mr-2"/>
+          <Skeleton width="100%" height="400px" className="mr-2" />
         ) : null}
         {isError && !loading ? (
-          <Suspense fallback = {<Skeleton width="100%" height="400px" className="mr-2"/>}>
-            <RetryQueryComponent message = "Error obteniendo los productos" refetch = {refetch}/>
+          <Suspense
+            fallback={<Skeleton width="100%" height="400px" className="mr-2" />}
+          >
+            <RetryQueryComponent
+              message="Error obteniendo los productos"
+              refetch={refetch}
+            />
           </Suspense>
-        ) : products.length === 0?
-        <Suspense fallback = {<Skeleton width="100%" height="400px" className="mr-2"/>}>
-            <RetryQueryComponent message = "No hay productos recomendados" refetch = {refetch}/>
+        ) : products.length === 0 ? (
+          <Suspense
+            fallback={<Skeleton width="100%" height="400px" className="mr-2" />}
+          >
+            <RetryQueryComponent
+              message="No hay productos recomendados"
+              refetch={refetch}
+            />
           </Suspense>
-        :(
+        ) : (
           <ImageSlider
             images={products.map((product) => ({
               src: product.product_img1,
@@ -43,10 +55,11 @@ export function RecommendedProducts() {
       </header>
       <section>
         <p>
-          Lorem ipsum dolor sit amet consectetur. Viverra at at lorem sed quam
-          tempor ac donec. Suspendisse nullam dignissim massa dui rhoncus diam
-          egestas urna consectetur. Mauris arcu nulla egestas aliquet fermentum.
-          Ut pretium suspendisse adipiscing eu ac.
+          Te presentamos lo mejor valorado, más buscado y comprado de nuestra tienda. 
+          Aquí encontrarás las últimas tendencias en moda, seleccionadas
+          cuidadosamente para satisfacer tus gustos y necesidades.  Desde prendas básicas hasta accesorios únicos,
+          cada artículo ha sido elegido por su originalidad y popularidad entre
+          nuestros clientes. <strong>¡Comienza a comprar ahora mismo!</strong>
         </p>
       </section>
     </article>
